c6ead340b9df4f551ccd0baba512f3d339f8156a
Belisnalva Costa <bella-costa@hotmail.com.br>
2021-11-20 10:37:48 -0300

    Commit initial

---
+++ PortariaBD/PortariaBD/src/main/java/NTTData/Jpa/PortariaBD/PortariaBdApplication.java
@@ -0,0 +1,13 @@
+package NTTData.Jpa.PortariaBD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortariaBdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortariaBdApplication.class, args);
+	}
+
+}
